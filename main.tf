bfb761d651d27ab1394f169a9cd691fb6e69b2ff
Dion Chamika <129253554+dionchamika@users.noreply.github.com>
2024-01-09 10:53:25 +0530

    CICD Commit

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "dion-tfstate-bkt"  # Change this to a globally unique name
-  acl    = "public"
+  acl    = "private"
 }
