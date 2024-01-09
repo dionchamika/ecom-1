6760b9f92c81ab7f320100ba173463c9d7d651c8
Dion Chamika <129253554+dionchamika@users.noreply.github.com>
2024-01-09 10:51:50 +0530

    CICD Commit

--- main.tf
+++ main.tf
@@ -2,20 +2,8 @@ provider "aws" {
   region = "us-east-1"  # Adjust region as needed
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  # Add necessary configurations
-}
-
-resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Choose an AMI
-  instance_type = "t2.micro"
-  # Add necessary configurations
-}
-
-resource "aws_db_instance" "main" {
-  identifier           = "mydbinstance"
-  allocated_storage    = 20
-  instance_class       = "db.t2.micro"
-  # Add necessary configurations
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "dion-tfstate-bkt"  # Change this to a globally unique name
+  acl    = "public"
 }
