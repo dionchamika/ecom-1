a582253fc21e82b10a78613af32ff794f424453c
Dion Chamika <129253554+dionchamika@users.noreply.github.com>
2024-01-12 00:42:16 +0530

    commit

--- main.tf
+++ main.tf
@@ -2,20 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "react4_app_server" {
+resource "aws_instance" "react2_app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "react4_app_server"
+    Name = "react2_app_server"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt update",
+      "sudo apt-get install -y git",
+      "git clone https://github.com/dionchamika/ecom-1.git",
+      "cd ecom-1",
+      "ls -la",
+      # Add any additional commands you need here to host your application
+    ]
   }
 }
-
-resource "aws_s3_bucket" "dion5-tfstate-bkt" {
-  bucket = "dion5-tfstate-bkt"
-  acl    = "private"
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
