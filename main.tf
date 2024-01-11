d794df7ed3a8392e9a26f096bcfa11ac6ce49bc9
Dion Chamika <129253554+dionchamika@users.noreply.github.com>
2024-01-11 20:57:28 +0530

    commit

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ resource "aws_s3_bucket" "dion5-tfstate-bkt" {
     Name = "Terraform State Bucket"
   }
 }
+
+resource "aws_db_instance" "my_database" {
+  identifier           = "my-db-instance"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydatabase"
+  username             = "admin"
+  password             = "admin12345"
+  
+  tags = {
+    Name = "MyDatabase"
+  }
+}
