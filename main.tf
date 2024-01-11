3925c23bc3b2260e24fcfefaa8de59b1303b9fea
Dion Chamika <129253554+dionchamika@users.noreply.github.com>
2024-01-11 20:59:56 +0530

    commit

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "react_app_server" {
+resource "aws_instance" "react2_app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "react_app_server"
+    Name = "react2_app_server"
   }
 }
 
-resource "aws_s3_bucket" "dion5-tfstate-bkt" {
-  bucket = "dion5-tfstate-bkt"
+resource "aws_s3_bucket" "dion6-tfstate-bkt" {
+  bucket = "dion6-tfstate-bkt"
   acl    = "private"
   tags = {
     Name = "Terraform State Bucket"
