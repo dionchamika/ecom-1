e65352ab8b1406a004900e641a79f4c252895215
Dion Chamika <129253554+dionchamika@users.noreply.github.com>
2024-01-12 02:24:25 +0530

    commit

--- main.tf
+++ main.tf
@@ -13,17 +13,15 @@ resource "aws_instance" "react2_app_server" {
 
 resource "aws_s3_bucket" "dion5-tfstate-bkt" {
   bucket = "dion5-tfstate-bkt"
-  
+
+  versioning {
+    enabled = true
+  }
+
   tags = {
     Name = "Terraform State Bucket"
   }
 }
-
-resource "aws_s3_bucket_versioning" "dion5-tfstate-bkt-versioning" {
-  bucket = aws_s3_bucket.dion5-tfstate-bkt.bucket
-  enabled = true
-}
-
 
 output "instance_id" {
   value = aws_instance.react2_app_server.id
